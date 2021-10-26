22abf039a263f96db784e9fa623147f8cbfd2186
ryuuzera <87572712+ryuuzera@users.noreply.github.com>
2021-10-26 20:42:05 -0300

    first commit

---
+++ vcl-autoservice-ui/projeto.dpr
@@ -0,0 +1,14 @@
+program projeto;
+
+uses
+  Vcl.Forms,
+  tela in 'tela.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
